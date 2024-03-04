f7cc962987cce1cb6d04ceba5061b22d4b72e09f
Tobibankole123 <162153115+Tobibankole123@users.noreply.github.com>
2024-03-04 16:42:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const countDownDate = new Date("March 4, 2024 00:00:00").getTime();
+
+function formatNumber(n) {
+  if (n < 10) return `0${n}`;
+
+  return n;
+}
+
+const x = setInterval(function () {
+  const now = new Date().getTime();
+
+  const distance = countDownDate - now;
+
+  // Get days, hours, minutes and seconds
+  const days = formatNumber(Math.floor(distance / (1000 * 60 * 60 * 24)));
+  const hours = formatNumber(
+    Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const minutes = formatNumber(
+    Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  );
+  const seconds = formatNumber(Math.floor((distance % (1000 * 60)) / 1000));
+
+  // Show values in the browser
+  document.getElementById(
+    "countdown"
+  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  // Show "EXPIRED" if the deadline has passed
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
